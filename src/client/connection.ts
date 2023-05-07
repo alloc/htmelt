d60c6103c1b5cbba74dcda706daec81a0d87d15c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-07 14:59:24 -0400

    fix(hmr): avoid double reconnect

--- src/client/connection.ts
+++ src/client/connection.ts
@@ -24,15 +24,20 @@ function connect() {
       connected = true
     }
   }
+  let reconnecting = false
+  const reconnect = () => {
+    if (!reconnecting) {
+      reconnecting = true
+      setTimeout(connect, 1000)
+    }
+  }
+  ws.onerror = reconnect
   ws.onclose = () => {
     if (connected) {
       console.log('[HMR] disconnected')
       connected = false
     }
-    setTimeout(connect, 1000)
-  }
-  ws.onerror = () => {
-    setTimeout(connect, 1000)
+    reconnect()
   }
 }
 
