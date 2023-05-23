34bcdb8bbc8d5e86a991f552d8d5aba25e00f230
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-22 20:11:48 -0400

    fix(webext): prevent @types/node from leaking into client types

--- packages/webext/client/index.d.ts
+++ packages/webext/client/index.d.ts
@@ -1,4 +1,4 @@
-import type { WebExtension } from './dist/types.mjs'
+import type { WebExtension } from '../dist/types.mjs'
 
 declare module '@htmelt/plugin/dist/importMeta.mjs' {
   export interface ImportMeta {
