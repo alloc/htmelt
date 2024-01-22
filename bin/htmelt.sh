a8c39d442f9bcc0fcf1566cf129cb8b32fe1dfaf
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-01-22 17:02:19 -0500

    chore: improve the bin script

--- bin/htmelt.sh
+++ bin/htmelt.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-function spawnCLI() {
-  local ENTRY="$(readlink -f "$0")/../../dist/cli.mjs"
+function findProgramPath() {
+  local expression="require('path').dirname(require('fs').realpathSync('$0'))"
+  local bin_dir=`node -e "process.stdout.write($expression)"`
 
-  # Enable source maps when executed via local clone.
-  if [[ "$ENTRY" != *"node_modules"* ]]; then
-    export NODE_OPTIONS="--enable-source-maps"
-  fi
-
-  node "$ENTRY" "$@" || spawnCLI "$@"
+  echo "$bin_dir/../dist/cli.mjs"
 }
 
-spawnCLI "$@"
+HTMELT_PATH="$(findProgramPath)"
+HTMELT_SPAWN_TIME="0"
+
+# Enable source maps when executed via local clone.
+if [[ "$HTMELT_PATH" != *"node_modules"* ]]; then
+  export NODE_OPTIONS="--enable-source-maps"
+  echo "Enabling source maps."
+fi
+
+function run() {
+  local now="$(date +%s)"
+  local delta="$(($now - $HTMELT_SPAWN_TIME))"
+
+  # Only respawn if >15 seconds have passed.
+  if [[ "$delta" -gt "15" ]]; then
+    HTMELT_SPAWN_TIME="$now"
+
+    node "$HTMELT_PATH" "$@" || run "$@"
+  fi
+}
+
+run "$@"
